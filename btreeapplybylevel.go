d3e1a738c3500701f1e43ab4d9ddac5bb5bb3268
argannaute <argann2003@gmail.com>
2022-04-28 14:16:22 +0300

    every

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -1,25 +1,47 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
-
-func BTreeApplyByLevel(root *TreeNode, fn interface{}) {
-	novo := BTreeLevelCount(root)
-	for d := 1; d <= novo; d++ {
-
-		PrintNodesLevel(root, d, fn)
+func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
+	h := BTreeLevelcount(root)
+	for i := 0; i < h; i++ {
+		applyLevel(root, i, f)
 	}
 }
-func PrintNodesLevel(root *TreeNode, level int, fn interface{}) {
-	if root != nil {
-		if level == 1 {
-			ar := []interface{}{root.Data}
-			z01.Call(fn, ar)
 
-		} else if level > 1 {
-			PrintNodesLevel(root.Left, level-1, fn)
-			PrintNodesLevel(root.Right, level-1, fn)
-		}
+func applyLevel(root *TreeNode, level int, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
 	}
+	if level == 0 {
+		f(root.Data)
+	} else if level > 0 {
+		applyLevel(root.Left, level-1, f)
+		applyLevel(root.Right, level-1, f)
+	}
+}
+
+func BTreeLevelcount(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := BTreeLevelcount(root.Left)
+	right := BTreeLevelcount(root.Right)
+
+	if left > right {
+		return left + 1
+	}
+	return right + 1
+}
+
+func BTreeInsertdata(root *TreeNode, data string) *TreeNode {
+	if root == nil {
+		return &TreeNode{Data: data}
+	}
+	if data < root.Data {
+		root.Left = BTreeInsertdata(root.Left, data)
+		root.Left.Parent = root
+	} else if data > root.Data {
+		root.Right = BTreeInsertdata(root.Right, data)
+		root.Right.Parent = root
+	}
+	return root
 }
